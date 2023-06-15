c296ae730768f1a3766aecb9b3468a886c8592c3
sleeppy07 <133948911+sleeppy07@users.noreply.github.com>
2023-06-15 13:49:35 +0700

    update
    update folder lesson6

--- Lesson6/MYSQLI.php
+++ Lesson6/MYSQLI.php
@@ -12,7 +12,7 @@
 
  //CREATE TABLE
 
- $sql_KH = "CREATE table KHACHHANG(
+ $sql_stmt_KH = "CREATE table KHACHHANG(
     MAKH char(4) not null PRIMARY key,
     HOTEN varchar(40) not null,
     DIACHI varchar(50) not null,
@@ -22,14 +22,14 @@
     NGDK datetime
     );";
 
-$sql_NV = "CREATE table NHANVIEN(
+$sql_stmt_NV = "CREATE table NHANVIEN(
     MANV char(4) not null PRIMARY key,
     HOTEN varchar(40) not null,
     NGVL datetime,
     SODT varchar(20) not null
     );";
 
-$sql_SP = "CREATE table SANPHAM(
+$sql_stmt_SP = "CREATE table SANPHAM(
     MASP char(4) not null PRIMARY key,
     TENSP varchar(40) not null,
     DVT varchar(10) not null,
@@ -37,7 +37,7 @@ $sql_SP = "CREATE table SANPHAM(
     GIA float
     );";
 
-$sql_HD = "CREATE table HOADON(
+$sql_stmt_HD = "CREATE table HOADON(
     SOHD char(4) not null PRIMARY key,
     NGHD datetime,
     MAKH char(4) not null,
@@ -47,7 +47,7 @@ $sql_HD = "CREATE table HOADON(
     FOREIGN KEY (MANV) REFERENCES NHANVIEN(MANV)
     );";
 
-$sql_CTHD = "CREATE table CTHD(
+$sql_stmt_CTHD = "CREATE table CTHD(
     MACTHD char(4) not null PRIMARY key,
     SOHD char(4) not null,
     MASP char(4) not null,
@@ -56,7 +56,7 @@ $sql_CTHD = "CREATE table CTHD(
     FOREIGN KEY (MASP) REFERENCES SANPHAM(MASP)
     );";
  
-$result = mysqli_query($dbh, $sql_KH); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_KH); // Thực thi câu lệnh SQL
 
 if (!$result) {
     die("Create failed table KHACHHANG: " . mysqli_error($dbh)); 
@@ -65,7 +65,7 @@ if (!$result) {
     echo "Create success KHACHHANG <br>";
 }
 
-$result = mysqli_query($dbh, $sql_NV); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_NV); // Thực thi câu lệnh SQL
 
 if (!$result) {
     die("Create failed table NHANVIEN: " . mysqli_error($dbh)); 
@@ -75,7 +75,7 @@ if (!$result) {
 }
 
 
-$result = mysqli_query($dbh, $sql_SP); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_SP); // Thực thi câu lệnh SQL
 
 if (!$result) {
     die("Create failed table SANPHAM: " . mysqli_error($dbh)); 
@@ -84,7 +84,7 @@ if (!$result) {
     echo "Create success SANPHAM <br>";
 }
 
-$result = mysqli_query($dbh, $sql_HD); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_HD); // Thực thi câu lệnh SQL
 
 if (!$result) {
     die("Create failed table HOADON: " . mysqli_error($dbh)); 
@@ -93,7 +93,7 @@ if (!$result) {
     echo "Create success HOADON <br>";
 }
 
-$result = mysqli_query($dbh, $sql_CTHD); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_CTHD); // Thực thi câu lệnh SQL
 
 if (!$result) {
     die("Create failed table CHITIETHOADON: " . mysqli_error($dbh)); 
@@ -104,7 +104,7 @@ if (!$result) {
 
 //INTERT DATA
 
-$sql_KH = "INSERT INTO khachhang (MAKH,HOTEN,DIACHI,SODT,NGAYSINH,DOANHSO,NGDK)
+$sql_stmt_KH = "INSERT INTO khachhang (MAKH,HOTEN,DIACHI,SODT,NGAYSINH,DOANHSO,NGDK)
                 values ('KH01','Nguyen Van A','Thanh Xuan, HN','0999888777','2002-10-01','13000000','2009-10-01'),
                 ('KH02','Nguyen Van B','Thanh Xuan, HN','0999888777','2002-08-01','5000000','2009-10-01'),
                 ('KH03','Nguyen Van C','Cau Giay, HN','0999888776','2002-07-01','6000000','2009-10-01'),
@@ -116,14 +116,14 @@ $sql_KH = "INSERT INTO khachhang (MAKH,HOTEN,DIACHI,SODT,NGAYSINH,DOANHSO,NGDK)
                 ('KH09','Nguyen Van I','Trung Hoa, HN','0999888770','2002-10-01','17000000','2009-10-01'),
                 ('KH10','Nguyen Van J','Hoan Kiem, HN','0999888769','2002-10-01','9000','2009-10-01')"; 
 
-$sql_NV = "INSERT INTO nhanvien (MANV,HOTEN,NGVL,SODT)
+$sql_stmt_NV = "INSERT INTO nhanvien (MANV,HOTEN,NGVL,SODT)
                 values ('NV01','Tran Van A','2009-10-01','0999888777'),
                 ('NV02','Tran Van B','2009-10-01','0999888777'),
                 ('NV03','Tran Van C','2009-10-01','0999888776'),
                 ('NV04','Tran Van D','2009-10-01','0999888775'),
                 ('NV05','Tran Van E','2009-10-01','0999888774')";
 
-$sql_SP = "INSERT INTO sanpham (MASP,TENSP,DVT,NUOCSX,GIA)
+$sql_stmt_SP = "INSERT INTO sanpham (MASP,TENSP,DVT,NUOCSX,GIA)
                 values ('BC01','But chi','cay','Viet Nam','3000'),
                 ('BC02','But chi','cay','Viet Nam','3000'),
                 ('BC03','But chi','cay','Sigapore','5000'),
@@ -133,7 +133,7 @@ $sql_SP = "INSERT INTO sanpham (MASP,TENSP,DVT,NUOCSX,GIA)
                 ('BB02','But bi','cay','Thai Lan','100000'),
                 ('BB03','But bi','hop','Trung Quoc','2000')";
 
-$sql_HD = "INSERT INTO hoadon (SOHD,NGHD,MAKH,MANV,TRIGIA)
+$sql_stmt_HD = "INSERT INTO hoadon (SOHD,NGHD,MAKH,MANV,TRIGIA)
                 values ('1001','2012-10-01','KH01','NV01','320000'),
                 ('1002','2012-10-01','KH01','NV02','530000'),
                 ('1003','2012-10-01','KH02','NV01','450000'),
@@ -143,7 +143,7 @@ $sql_HD = "INSERT INTO hoadon (SOHD,NGHD,MAKH,MANV,TRIGIA)
                 ('1007','2012-10-01','KH03','NV03','1000000'),
                 ('1008','2012-10-01','KH01','NV03','230000')";
 
-$result = mysqli_query($dbh, $sql_KH); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_KH); // Thực thi câu lệnh SQL
     
 if (!$result) {
     die("Add failed table KHACHHANG: " . mysqli_error($dbh)); 
@@ -153,7 +153,7 @@ if (!$result) {
 }
 
 
-$result = mysqli_query($dbh, $sql_NV); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_NV); // Thực thi câu lệnh SQL
     
 if (!$result) {
     die("Add failed table NHANVIEN: " . mysqli_error($dbh)); 
@@ -163,7 +163,7 @@ if (!$result) {
 }
 
 
-$result = mysqli_query($dbh, $sql_SP); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_SP); // Thực thi câu lệnh SQL
     
 if (!$result) {
     die("Add failed table SANPHAM: " . mysqli_error($dbh)); 
@@ -173,7 +173,7 @@ if (!$result) {
 }
 
 
-$result = mysqli_query($dbh, $sql_HD); // Thực thi câu lệnh SQL
+$result = mysqli_query($dbh, $sql_stmt_HD); // Thực thi câu lệnh SQL
     
 if (!$result) {
     die("Add failed table HOADON: " . mysqli_error($dbh)); 
@@ -182,5 +182,28 @@ if (!$result) {
     echo "Add success table HOADON <br>";
 }
 
+// UPDATE DATA
+$sql_stmt = "UPDATE khachhang set HOTEN = 'NguyenThuHa222' where MAKH = 'KH01'";
+
+$result = mysqli_query($dbh, $sql_stmt); // Thực thi câu lệnh SQL
+    
+if (!$result) {
+    die("Update failed: " . mysqli_error($dbh)); 
+    // Thông báo lỗi nếu thực thi câu lệnh thất bại
+} else {
+    echo "Update success";
+}
+
+//delete
+$sql_stmt = "DELETE from khachhang where MAKH = 'KH10'";
+
+$result = mysqli_query($dbh, $sql_stmt); // Thực thi câu lệnh SQL
+    
+if (!$result) {
+    die("Delete failed: " . mysqli_error($dbh)); 
+    // Thông báo lỗi nếu thực thi câu lệnh thất bại
+} else {
+    echo "Delete success";
+}
 
 ?>
